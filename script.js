c7a59ee6961def61edb116ed5f4d54399c9ce07d
Imke Högden <imke.hoegden@dci-students.org>
2025-11-22 18:28:19 +0100

    Änderungen vom 22. Nov 2025

---
+++ script.js
@@ -0,0 +1,9 @@
+/*JS für Hamburger Menü */
+
+const hamburger = document.querySelector(".hamburger");
+const navbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navbar.classList.toggle("active");
+});
