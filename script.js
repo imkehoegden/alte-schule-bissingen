99f0d03252d433b12dd4094851fa1586cce46f36
imkehoegden <imke.hoegden@posteo.de>
2026-04-07 17:22:02 +0200

    Fix: JS Belegungsübersicht modifiziert

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*JS für Hamburger Menü */
+/* Hamburger Menü */
 
 const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".navbar");
@@ -8,7 +8,7 @@ hamburger.addEventListener("click", () => {
   navbar.classList.toggle("active");
 });
 
-/* Google-Kalender / Belegungsübersicht laden */
+/* Google-Kalender / Belegungsübersicht */
 function loadCalendar() {
   document.getElementById("calendar").innerHTML =
     '<iframe src="https://calendar.google.com/calendar/embed?src=c_3cefeec9444682c8c041438c3617d1929b203faa276b41f3b27dbbf1a57d6fd5%40group.calendar.google.com&ctz=Europe%2FBerlin" style="border: 0" width="870" height="600" frameborder="0" scrolling="no" class="google-kalender" ></iframe>';
